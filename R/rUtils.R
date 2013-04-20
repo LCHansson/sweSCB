125c79ff2de90b64b052e50f4f005c5376ccf604
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-04-20 13:41:39 +0200

    Fix getContent

--- R/rUtils.R
+++ R/rUtils.R
@@ -7,3 +7,33 @@
 buildPath <- function(queryData = NULL) {
 	paste(queryData, collapse="/")
 }
+
+#' Get content from response
+#' 
+#' Get the content from a response object
+#' 
+#' @param response response object
+#' @param type type format
+#' 
+getContent <- function(response, type = "csv") {
+    
+    if (!class(response) == "response") {
+        stop("needs to be an response class object")
+    }
+    
+    # Convert to character
+    content <- httr::content(response)
+    
+    if (type == "csv") {
+        content <- read.table(
+            textConnection(content), 
+            sep = ",", 
+            header = T, 
+            stringsAsFactors = F
+        )
+    } else {    
+        stop("Unsupported type format")
+    }
+    
+    return(content)
+}

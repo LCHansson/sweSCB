037be0b0386ce43ffacbc111c9e1bd4f8d689f37
Love Hansson <love.hansson@gmail.com>
2013-04-23 23:28:56 +0200

    Moved examples from main file to examples.R. All three examples should be working now...

--- R/rSCBapi.R
+++ R/rSCBapi.R
@@ -8,17 +8,6 @@
 #' @name rSCBapi
 #' @docType package
 
-scbQueryData <- list(
-	base_url = "http://api.scb.se",
-	api_name = "OV0104", # Are there any other names?
-	api_version = "v1",
-	database_id = "doris",
-	language = "sv",
-	levels = "ssd",
-	table_id = "BE/BE0401/"
-)
-
-baseUrl <- buildPath(scbQueryData)
 
 
-
+# NULL
--- examples.R
+++ examples.R
@@ -1,3 +1,40 @@
+require(httr)
+require(RJSONIO)
+
+
+
+##### EXAMPLE: HIERARCHY MINING #####
+
+# Define the query
+scbQueryData <- list(
+	base_url = "http://api.scb.se",
+	api_name = "OV0104", # Are there any other names?
+	api_version = "v1",
+	database_id = "doris",
+	language = "sv",
+	levels = "ssd",
+# 	table_id = "AM/AM0114"
+		table_id = "AM/AM0106/AM0106B/Kommun15g"
+)
+
+# Build the base URL
+baseUrl <- buildPath(scbQueryData)
+
+# Get sublevels at the current node
+getLevels(baseUrl)
+
+# Then get the data
+# This won't work... yet
+getScbData(baseUrl)
+
+# We probably also want to be able to recursively describe all subnodes at
+# a certain node.
+# (This also won't work yet)
+getAllLevelsBelow(baseUrl)
+
+
+
+##### EXAMPLE: FETCHING ACTUAL DATA #####
 scbQueryData <- list(
 	base_url = "http://api.scb.se",
 	api_name = "OV0104", # Are there any other names?
@@ -35,6 +72,16 @@ POST(
 )
 
 
-## Parsa output till en data.frame när output är ett CSV-objekt
+## Parse the CSV output to a data.frame and print it
 a <- content(response, as="text")
 b <- read.table(textConnection(a), sep=',', header=T, stringsAsFactors=F)
+print(b)
+
+
+
+
+##### EXAMPLE: INSTALLING THE rSCBapi PACKAGE #####
+require(devtools)
+
+install_github("rSCBapi", "prenumerant")
+

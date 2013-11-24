8d1e8016daf6e4fe736169cbdf7d0a33c3ebac57
Love Hansson <love.hansson@gmail.com>
2013-11-24 18:36:27 +0100

    Code cleanup in examples

--- inst/examples/ex_data_mining.R
+++ inst/examples/ex_data_mining.R
@@ -1,50 +1,38 @@
-## EXAMPLE: USING rSCBapi TO MINE DATA FROM THE SCB API
+## EXAMPLE: USING rSCB TO MINE DATA FROM THE SCB API
 
 ## 1. Get metadata from the base node
-a <- scbGetMetadata(buildPath(baseURL()))
+a <- scbGetMetadata()
 
 # View the data. We will recycle the values in the "id" column to delve deeper into the API node structure.
 View(a)
 
 # Go down one level into the node tree.
-b <- scbGetMetadata(buildPath(baseURL(),
-                              as.character(a$id[9])
+b <- scbGetMetadata(a$id[1])
 
-))
 # Do this for consecutive steps...
-cc <- scbGetMetadata(buildPath(baseURL(),
-                               as.character(a$id[9]),
-                               as.character(b$id[4])
-))
+cc <- scbGetMetadata(b$id[5])
 
 # ...until we reach a node with no nodes below it, only data
-d <- scbGetMetadata(buildPath(baseURL(),
-                              as.character(a$id[9]),
-                              as.character(b$id[4]),
-                              as.character(cc$id[1])
-))
+d <- scbGetMetadata(cc$id[3])
 
-# Construct the URL to the data
-dataURL <- buildPath(baseURL(),
-                     as.character(a$id[9]),
-                     as.character(b$id[4]),
-                     as.character(cc$id[1]),
-                     as.character(d$id[1])
-)
+datanode <- scbGetMetadata(d$id[1])
 
-# Get the dimensions of the data
-e <- scbGetDims(dataURL)
+# We receive a message that this is a bottom node. We can now use d$URL to get dimensions of the data:
+dims  <- scbGetDims(datanode)
 
 # View dimension names
-for(i in 1:length(e)) {
-   print(e[[i]]$code)
+for(i in 1:length(dims)) {
+   print(dims[[i]]$code)
 }
 
 # View dimension value space
-for(i in 1:length(e)) {
-   print(e[[i]]$code)
-   print(e[[i]]$values)
+for(i in 1:length(dims)) {
+   print(dims[[i]]$code)
+   print(dims[[i]]$values)
 }
 
 # Submit a query containing one or several values for each of the named dimensions
-sdata <- scbGetData(dataURL, list(ContentsCode = "*", Tid = "1980M01"))
+sdata <- scbGetData(datanode$URL, list(ContentsCode = "AM0110D1", Utbildningsgrupp = "*", Kon = "*", Tid = "2012"))
+
+# Create a simple bar plot with the resulting data
+ggplot(sdata,aes(x=utbildningsgrupp.SUN.2000,y=Grundlön.2012,fill=kön)) + geom_bar(position=position_dodge()) + coord_flip()
--- inst/examples/ex_get_data.R
+++ inst/examples/ex_get_data.R
@@ -1,13 +1,30 @@
 # Build url
-url <- (buildPath(baseURL(), "BefProgFoddaMedel10"))
+varname <- "BefProgFoddaMedel10"
+
+# Get metadata
+metadata <- scbGetMetadata(varname)
 
 # Get dimensions (names of dimensions are printed in the terminal)
-dims <- scbGetDims(url)
+dims <- scbGetDims(metadata)
 
 # Get data
-test <- scbGetData(url, dims=list(
+test <- scbGetData(metadata$URL, dims=list(
 	Fodelseland = "010",
 	Alder="*",
 	ContentsCode = "*",
 	Tid="*"
 ))
+
+# Examine data
+View(test)
+
+# Make sure the data is actually numeric (it often isn't)
+if(is.character(test$Födda.2010)) {
+	for(i in 1:nrow(test)) {
+		test$Födda.2010[i] <- paste0(str_extract_all(test$Födda.2010[i],"[[:digit:]]")[[1]],collapse="")
+	}
+	test$Födda.2010 <- as.numeric(test$Födda.2010)
+}
+	
+# Plot the data
+ggplot(test,aes(x=ålder,y=Födda.2010,fill=födelseland)) + geom_histogram()

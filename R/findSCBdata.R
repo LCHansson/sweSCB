9f6871c592203a5c7d93eb51e400df53b4a3df88
Måns Magnusson <manma97@Manss-MacBook-Pro.local>
2013-11-28 10:22:37 +0100

    findSCBdata draft2

--- R/findSCBdata.R
+++ R/findSCBdata.R
@@ -1,17 +1,18 @@
-# To do: 
-# Lägg in att välja variabler
-# Lägg in assertions
-# Lägg in att clean and convert med reshape/plyr
-
-
-
-#' Wrapper function to simply find and download data from SCB to the current R session.
+#' Wrapper function to simply find and download data from SCB to the current R session. 
 #' 
 #' @param history keep the history when the function is running.
 #' 
 #' @examples
 #' 
+#' @seealso
+#' \code{\link{scbGetMetadata}}, \code{\link{getData}}
+#' 
 #' @export
+
+# To do: 
+# Lägg in assertions
+# Lägg in att clean and convert med reshape/plyr (to wide format)
+# Put clean in download data function instead
 
 findSCBdata<-function(history=FALSE){
   Node <- rSCB::scbGetMetadata() # Get top node
@@ -21,7 +22,7 @@ findSCBdata<-function(history=FALSE){
   while(!quit){ # The main program
     # Generate header
     if(!history){cat("\014")}
-    cat("CONTENT OF SCB API AT CURRENT (",length(allNodes)+1,") LEVEL:\n",sep="") 
+    cat("CONTENT OF SCB API AT CURRENT (",length(allNodes)+1,") NODE LEVEL:\n",sep="") 
     cat(rep("=",getOption("width")),"\n",sep="") 
     
     # Print information in node and ask for choice
@@ -38,7 +39,7 @@ findSCBdata<-function(history=FALSE){
     
     if(str_detect(inputValue,pattern="[0-9]+")){
       if(Node$type[as.numeric(inputValue)]=="t"){
-        .findScbData.Download(scbGetMetadata(Node$id[as.numeric(inputValue)]))
+        .findScbData.Download(rSCB::scbGetMetadata(Node$id[as.numeric(inputValue)]))
         
         inputDownMore<-.findScbData.input(type="yesno",
             input="\nDo you want to download more data from SCB?")
@@ -53,6 +54,7 @@ findSCBdata<-function(history=FALSE){
 
 
 .findScbData.Download <- function(dataNode){
+  
   inputDown<-.findScbData.input(type="yesno",
       input="\nDo you want to download this file?")
   if(inputDown=="n"){return()}
@@ -64,28 +66,50 @@ findSCBdata<-function(history=FALSE){
   inputName<-.findScbData.input(type="text",
       input="\nLoad data.frame into R as (ex. myData):")
   
-  # The rest is to do
-    varList<-list()
-    varListText<-c("")
-    for(listElem in dataNode$variables$variables){
-      varList[[listElem$code]]<-"*"
-      varListText<-c(varListText,str_c(listElem$code," = \"*\", ",sep=""))
-    }
-    varListText[length(varListText)]<-str_sub(varListText[length(varListText)],1,
-                         str_length(varListText[length(varListText)])-2)
-    varListText <- varListText[-1]
-
-    tempData<-scbGetData(dataNode$URL, varList)
-    if(clean){tempData<-scbCleanData(tempData)}
-    assign(inputName,value=scbGetData(dataNode$URL, varList),envir=.GlobalEnv)
+  # Choose variables values
+  varList<-list()
+  varListText<-character(0)
+  
+  for(i in 1:length(dataNode$variables$variables)){
+    listElem <- dataNode$variables$variables[[i]]
+    varDF <- data.frame(id=listElem$values,text=listElem$valueTexts,stringsAsFactors=FALSE)
     
+    varAlt <- .findScbData.input(type="alt",input=list(varDF,listElem$text))
+    
+    if(varAlt[1]!="*"){
+      tempAlt <- character(0)
+      for (i in 1:length(varAlt)){ 
+        index <- as.numeric(unlist(str_split(varAlt[i],pattern=":")))
+        if(length(index)==2){
+          tempAlt <- c(tempAlt,listElem$values[index[1]:index[2]])
+        }else{
+          tempAlt <- c(tempAlt,listElem$values[index])
+        }  
+      }
+    }else{
+      tempAlt <- "*"
+    }
+    
+    varList[[listElem$code]] <- tempAlt    
+    varListText<-c(varListText,
+                   str_c(listElem$code," = c('",
+                         str_c(tempAlt,collapse="', '"),
+                         "')",collapse=""))
+  }
+  cat("Downloading... ")
+  tempData<-rSCB::scbGetData(dataNode$URL, varList)
+  cat("Done.\n")
+  if(clean){tempData<-rSCB::scbCleanData(tempData)}
+  assign(inputName,value=tempData,envir=.GlobalEnv)
+    
+    # Redo this part as own printout function
     cat("The data has been downloaded and loaded into R with the following code:\n\n")
     cat(inputName," <- \n  scbGetData(\"", dataNode$URL,"\",\n",
         rep(" ",13), "list(",sep="")
     for (i in 1:length(varListText)){
       if(i!=1){cat(rep(" ",18),sep="")}
       cat(varListText[i],sep="")
-      if(i!=length(varListText)){cat("\n",sep="")}
+      if(i!=length(varListText)){cat(",\n",sep="")}
     }
     cat("))\n")
     if(clean){
@@ -94,32 +118,33 @@ findSCBdata<-function(history=FALSE){
     cat("\n",sep="")
 }
 
-.findScbData.inputBaseCat <- function(alt){
+.findScbData.inputBaseCat <- function(alt,codedAlt){
   output<-"\n(" 
   for (i in 1:length(alt)){
-    if(i!=1){output <- str_join(output,", ",sep="")}
-    output <- str_join(output,"'",codedAlt[alt[i],1],"' = ",
-                       codedAlt[alt[i],2],sep="")
+    if(i!=1){output <- str_join(output, ", ", sep="")}
+    output <- str_join(output, "'", codedAlt[alt[i],1], "' = ",
+                       codedAlt[alt[i],2], sep="")
   }
-  return(str_join(output,")",sep=""))
+  return(str_join(output,")", sep=""))
 }
 
 
 .findScbData.input <- function(type=c("node","alt","yesno","text"),input=NULL){
-  codedAlt<-data.frame(abbr=c("q","b","*","y","n"),
-                       name=c("Quit","Back","All","Yes","No"),
-                       stringsAsFactors=FALSE)
+  codedAlt <- data.frame(abbr=c("q","b","*","y","n","a"),
+                         name=c("Quit","Back","Select all","Yes","No","Show all"),
+                         stringsAsFactors=FALSE)
+  textTitle<-alt<-character(0)
+  baseCat<-numeric(0)
   
   if(type=="node"){
     baseCat<-1:2
-    alt<-as.character(1:nrow(input))
-    textHead<-str_join("\nEnter the data (number) you want to explore:",
-                       .findScbData.inputBaseCat(baseCat),sep="")
+    alt<-rownames(input)
+    textHead<-"\nEnter the data (number) you want to explore:"
   }
   
   if(type=="yesno"){
     baseCat<-4:5
-    textHead<-str_join(input,.findScbData.inputBaseCat(baseCat),sep="")
+    textHead<-input
   }
   
   if(type=="text"){
@@ -127,90 +152,98 @@ findSCBdata<-function(history=FALSE){
   }
   
   if(type=="alt"){
-    baseCat<-1:3
-    alt<-input
-    altText <- .findScbData.printAltText(alt)
+    baseCat<-c(3,6)
+    varDF<-input[[1]]
+    alt<-rownames(varDF)
+    # Calculate a short list of alternatives
+    if(nrow(varDF)>11){
+      varDFshort<-varDF[c(1:6,(nrow(varDF)-4):nrow(varDF)),]
+      rownames(varDFshort)[6]<-"."
+    }else{
+      varDFshort<-varDF}
+
+    textTitle <-str_join("\nALTERNATIVES FOR VARIABLE: ",toupper(input[[2]])," \n",
+        str_join(rep("=",getOption("width")),collapse=""),"\n", sep="")    
     textHead <-
-      str_join("\nChoose your alternative(s), separate multiple choices by ','",
-      "\nAlternatives: ", altText,.findScbData.inputBaseCat(baseCat),sep="")
+      str_join("Choose your alternative(s) by number:",
+               "\nSeparate multiple choices by ',' and intervals by ':'",
+               sep="")
   }
 
   inputOK <- FALSE
+  inputScan <- ""
   
   while(!inputOK){
+    cat(textTitle)
+    if(type=="alt"){
+      if(inputScan=="a"){toprint <- varDF}else{toprint <- varDFshort}
+      .findScbData.printNode(xscb=toprint,print=TRUE)
+    }
     cat(textHead)
-    input<-scan(what=character(),nmax=1,multi.line = FALSE,quiet=TRUE)    
-    input<-tolower(str_trim(unlist(str_split(string=input,pattern=","))))
-    inputOK<-all(input%in%tolower(c(alt,codedAlt$abbr[baseCat]))) | type == "text"
-    if(!inputOK){cat("Sorry, no such entry allowed. Try again!")}
-  }
-  return(input)
+    if(type!="text"){cat(.findScbData.inputBaseCat(baseCat,codedAlt),"\n")}
+    
+    inputScan <- scan(what=character(), nmax=1, multi.line = FALSE, quiet=TRUE)    
+    inputScan <- tolower(str_trim(unlist(str_split(string=inputScan,pattern=","))))
+    if(inputScan[1]=="a"){next()}
+    
+    inputOK <- 
+      (length(inputScan) == 1 && inputScan %in% tolower(codedAlt$abbr[baseCat])) |
+      all(inputScan %in% tolower(alt) | 
+          str_detect(string=inputScan,pattern="[0-9]+:[0-9]+")) |
+      type == "text"
+        
+    if(!inputOK){cat("Sorry, no such entry allowed. Please try again!\n")}
+  } 
+  return(inputScan)
 }
 
-.findScbData.printAltText<-function(alt){  
-  alt <- as.character(alt)
-  noAlt <- length(alt)
-  noPos <- getOption("width") - 30
-  if (noAlt==1){
-    altText<-alt
-  }else if(noAlt==2){
-    altText<-str_join(alt,collapse=", ")
-  }else{
-    altTextFirst <- altTextLast <- c("")
-    first <- TRUE
-    while(sum(str_length(c(altTextFirst,altTextLast))) < noPos & length(alt) > 0){
-      if(first){
-        altTextFirst <- str_join(c(altTextFirst,alt[1]),collapse=", ")
-        alt <- alt[-1]
-      }else{
-        altTextLast <- str_join(c(alt[length(alt)],altTextLast),collapse=", ")
-        alt <- alt[-length(alt)]
-      }
-      first<-!first
-    }
-    altTextFirst <- str_sub(altTextFirst,start=3)
-    altTextLast <- str_sub(altTextLast,end=str_length(altTextLast)-2)
-    if(length(alt)==0){diff<-", "}else{diff<-" ... "}
-    altText<-str_join(c(altTextFirst,altTextLast),collapse=diff)
-  }
-  return(altText)
-}
-  
-.findScbData.printNode<-function(xscb){
+.findScbData.printNode<-function(xscb,print=TRUE){
   xscb$text<-as.character(xscb$text)
   nSCBidlen<-max(str_length(as.character(xscb$id)))
-  nSCBpos<-floor(log(x=nrow(xscb),10))+1
+  nSCBpos<-max(str_length(rownames(xscb)))
   nSCBconsole<-getOption("width")
   
   startPos<-nSCBpos+nSCBidlen+5
   scbTextSpace<-nSCBconsole-startPos
-  
+  finalText<-character(0) 
   for (i in 1:nrow(xscb)){
-    cat(i,".",rep(" ",nSCBpos-str_length(as.character(i))),
-        " [",xscb$id[i],"]",rep(" ",nSCBidlen-str_length(as.character(xscb$id[i]))),
-        " ",
-        sep="")
+    if(rownames(xscb)[i]=="."){
+      finalText <- str_join(finalText,"\n")
+      next()
+    }
+    
+    finalText <- str_join(
+      finalText,
+      rownames(xscb)[i],".",
+      str_join(rep(" ",nSCBpos-str_length(rownames(xscb)[i])),collapse=""),
+      " [",xscb$id[i],"]",
+      str_join(rep(" ",nSCBidlen-str_length(as.character(xscb$id[i]))),collapse=""),
+      " ",collapse="")
     
     first<-rerun<-TRUE
     tempText<-xscb$text[i]
     while(first | rerun){
       tempTextSpaces<-str_locate_all(tempText,pattern=" ")[[1]][,1]
-      if(str_length(tempText)>scbTextSpace){
+      if(str_length(tempText) > scbTextSpace){
         tempTextCut<-max(tempTextSpaces[tempTextSpaces < scbTextSpace])-1
       }else{
         tempTextCut<-str_length(tempText)
         rerun<-FALSE
       }
             
-      cat(rep(" ",startPos*(1-as.numeric(first))),
-          str_sub(tempText,1,tempTextCut),"\n",sep="")
+      finalText <-
+        str_join(finalText,
+                 str_join(rep(" ",startPos*(1-as.numeric(first))),collapse=""),
+                 str_sub(tempText,1,tempTextCut),"\n",collapse="")
       
       if(rerun){
-        tempText<-str_sub(tempText,tempTextCut+2)}
+        tempText<-str_sub(tempText,tempTextCut+2)
+      }
 
       first<-FALSE      
     }
   }
+  if(print){cat(finalText)}else{return(finalText)}
 }
 
 

4abb9f57f63e28f077f01d495adaae84d63871b1
Love Hansson <love.hansson@pensionsmyndigheten.se>
2014-02-04 13:12:54 +0100

    Remote buildPath() from NAMESPACE

--- R/scbUtils.R
+++ R/scbUtils.R
@@ -6,7 +6,7 @@
 #' @param topnodes A string or a list of strings containing the top nodes \emph{in top-to-bottom order}
 #' @param baseUrl The base URL to use. This is only useful if you want to use the function for constructing a URL to another web service or if SCB should suddenly change their base URL. If you want to pass arguments to \code{baseURL()}, use the \code{...} argument instead.
 #' @param ... Further arguments passed to  \code{baseURL()}.
-#' @export
+#' 
 
 buildPath <- function(varname, topnodes = NULL, baseUrl = NULL, ...) {
 	if (is.null(baseUrl))
